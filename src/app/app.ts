a2dee9233ef6018ea53ca362d10e6bc829d3011d
kylianjoff <kylianju382@gmail.com>
2025-12-11 15:27:19 +0100

    Création des interfaces pour les projets et les états avec les noms des états

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,37 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+export enum Etat {
+  EN_DEV,
+  EN_LIGNE,
+  NON_PUBLIE,
+  EN_PAUSE,
+  ANNULE
+}
+
+export interface Projet {
+  logo: string;
+  titre: string;
+  description: string;
+  etat: Etat;
+  dateCreation: Date;
+  dateMAJ: Date;
+}
+
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
 export class App {
   protected readonly title = signal('projets-kylianjulia');
+
+  nomEtat: string[] = [
+    "En développement",
+    "En ligne",
+    "Projet non publié",
+    "Projet actuellement en pause",
+    "Développement annulé"
+  ];
 }
