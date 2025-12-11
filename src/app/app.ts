d19e1afd3c0842bea4536dc72ab4ffede7632d9e
kylianjoff <kylianju382@gmail.com>
2025-12-11 23:01:06 +0100

    listing projets

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 export enum Etat {
   EN_DEV,
@@ -20,7 +21,7 @@ export interface Projet {
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
@@ -34,4 +35,23 @@ export class App {
     "Projet actuellement en pause",
     "Développement annulé"
   ];
+
+  projets: Projet[] = [
+    {
+      logo: "image",
+      titre: "projet1",
+      description: "projet qui sert à quelque chose",
+      etat: Etat.EN_LIGNE,
+      dateCreation: new Date('2025-12-11'),
+      dateMAJ: new Date('2025-12-11')
+    },
+    {
+      logo: "image",
+      titre: "projet2",
+      description: "lui il sert à ça",
+      etat: Etat.EN_DEV,
+      dateCreation: new Date('2025-11-28'),
+      dateMAJ: new Date('2025-12-10')
+    }
+  ]
 }
