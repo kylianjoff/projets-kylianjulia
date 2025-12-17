7338e66b9f3e786276629548b84b239af4657946
kylianjoff <kylianju382@gmail.com>
2025-12-17 14:42:34 +0100

    Initialisation des routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { App } from './app';
+import { Project } from './project/project';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: App,
+        children: [
+            {
+                path: 'project/:id',
+                component: Project
+            }
+        ]
+    }
+];
