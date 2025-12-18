15329b388a17cbd9cee26e5da489341683c11450
kylianjoff <kylianju382@gmail.com>
2025-12-19 00:52:52 +0100

    Maquette simple

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,23 +1,7 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal, OnInit } from '@angular/core';
+import { RouterOutlet, Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
-
-export enum Etat {
-  EN_DEV,
-  EN_LIGNE,
-  NON_PUBLIE,
-  EN_PAUSE,
-  ANNULE
-}
-
-export interface Projet {
-  logo: string;
-  titre: string;
-  description: string;
-  etat: Etat;
-  dateCreation: Date;
-  dateMAJ: Date;
-}
+import { ProjetService } from './services/projects.service';
 
 @Component({
   selector: 'app-root',
@@ -28,30 +12,16 @@ export interface Projet {
 export class App {
   protected readonly title = signal('projets-kylianjulia');
 
-  nomEtat: string[] = [
-    "En développement",
-    "En ligne",
-    "Projet non publié",
-    "Projet actuellement en pause",
-    "Développement annulé"
-  ];
+  constructor(
+    public projetService: ProjetService,
+    private router: Router
+  ) {}
 
-  projets: Projet[] = [
-    {
-      logo: "image",
-      titre: "projet1",
-      description: "projet qui sert à quelque chose",
-      etat: Etat.EN_LIGNE,
-      dateCreation: new Date('2025-12-11'),
-      dateMAJ: new Date('2025-12-11')
-    },
-    {
-      logo: "image",
-      titre: "projet2",
-      description: "lui il sert à ça",
-      etat: Etat.EN_DEV,
-      dateCreation: new Date('2025-11-28'),
-      dateMAJ: new Date('2025-12-10')
-    }
-  ]
+  openProjet(id:number) {
+    this.router.navigate(['/project', id]);
+  }
+
+  getEtatLabel(etat: number): string {
+    return this.projetService.nomEtat[etat];
+  }
 }
--- src/app/project/project.ts
+++ src/app/project/project.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, computed } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProjetService } from '../services/projects.service';
+import { Projet } from '../models/projet.model';
 
 @Component({
   selector: 'app-project',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './project.html',
   styleUrl: './project.css',
 })
 export class Project {
+  projet: Projet | undefined;
 
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    public projetService: ProjetService
+  ) {}
+
+  ngOnInit() {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.projet = this.projetService.getProjetById(id);
+  }
+
+  closeModal() {
+    this.router.navigate(['/']);
+  }
 }
