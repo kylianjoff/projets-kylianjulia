d43c385ab33589401b4e0772e81038b93bd7d2d3
kylianjoff <kylianju382@gmail.com>
2025-12-25 20:37:10 +0100

    bordel avec les token

---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+export const environment = {
+  production: false,
+  github: {
+    username: 'kylianjoff',
+    token: ''  // Vide par défaut
+  },
+  gitlab: {
+    username: 'kylianju382',
+    token: ''  // Vide par défaut
+  },
+  gitlabIsima: {
+    baseUrl: 'https://gitlab.isima.fr',
+    username: 'kyjulia',
+    token: ''  // Vide par défaut
+  }
+};
