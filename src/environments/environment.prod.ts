7bced96f9e0b162a834aff7a2d01ece39c9d4c67
kylianjoff <kylianju382@gmail.com>
2025-12-25 20:28:42 +0100

    ajout de l'environment prod

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,16 @@
+export const environment = {
+    production: true,
+    github: {
+        username: 'kylianjoff',
+        token: '' // Vide en production !
+    },
+    gitlab: {
+        username: 'kylianju382',
+        token: '' // Vide en production !
+    },
+    gitlabIsima: {
+        baseUrl: 'https://gitlab.isima.fr',
+        username: 'kyjulia',
+        token: '' // Vide en production !
+    }
+};
---
+++ src/environments/environment.template.ts
@@ -0,0 +1,16 @@
+export const environment = {
+  production: false,
+  github: {
+    username: 'kylianjoff',
+    token: ''  // À remplir en local
+  },
+  gitlab: {
+    username: 'kylianju382',
+    token: ''  // À remplir en local
+  },
+  gitlabIsima: {
+    baseUrl: 'https://gitlab.isima.fr',
+    username: 'kyjulia',
+    token: ''  // À remplir en local
+  }
+};
